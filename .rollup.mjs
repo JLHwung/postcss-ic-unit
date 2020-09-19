61b34b6d6986a806d242024fdee15f0642cdd375
Huáng Jùnliàng <jlhwung@gmail.com>
2020-09-19 14:46:32 -0400

    chore: remove unused external

--- .rollup.mjs
+++ .rollup.mjs
@@ -4,9 +4,8 @@ export default {
 	input: "index.mjs",
 	output: [
 		{ file: "dist/index.bundle.cjs", format: "cjs", exports: "default" },
-		{ file: "dist/index.bundle.mjs", format: "es" }
+		{ file: "dist/index.bundle.mjs", format: "es" },
 	],
-	external: ["browserslist", "caniuse-lite"],
 	plugins: [
 		babel.default({
 			babelHelpers: "bundled",
